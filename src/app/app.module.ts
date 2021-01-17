cdc657ab54243cc49327ee46bb9d6a35e47c55f1
meezan-mallick <meezan.mallick@gmail.com>
2021-01-17 15:07:48 +0530

    all components created and navbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NavbarComponent } from './navbar/navbar.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,NavbarComponent]
 })
 export class AppModule { }
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter,  OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  @Output() featureSelected= new EventEmitter<string>();
+  ngOnInit() {
+  }
+  onSelect(feature:string)
+  {
+    this.featureSelected.emit(feature);
+  }
+
+}
